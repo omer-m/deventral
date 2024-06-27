9d3f0cb8cd12f2de1c8a2a128c2942e5532c10fd
omer-shahid <bse183131@cust.pk>
2024-06-27 15:47:19 +0100

    Reapply .gitignore and clean up tracked files

--- config/database.py
+++
@@ -1,8 +0,0 @@
-from pymongo.mongo_client import MongoClient
-
-uri = "mongodb+srv://admin:Password1@cluster0.4uf5yal.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-# Create a new client and connect to the server
-client = MongoClient(uri)
-db = client['dataquality_DB']
-collection = db['files']
